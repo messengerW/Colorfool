f04bf19f8e8463e8739bb28ffd7b95757b0d188f
messengerW <messenger232624@gmail.com>
2019-09-23 21:29:28 +0800

    改

--- app/src/main/java/com/example/mushr/colorfool/MainActivity.java
+++ app/src/main/java/com/example/mushr/colorfool/MainActivity.java
@@ -10,17 +10,17 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView mTextMessage;
 
-    private TextView touchedXY, invertedXY, imgSize, colorRGB;
-    private ImageView imgSource1, imgSource2;
+    private TextView touchedXY, invertedXY, imgSize, colorText;
+    private ImageView imgSource1;
+    private Button colorBtn;
 
 
     @Override
@@ -31,31 +31,23 @@ public class MainActivity extends AppCompatActivity {
         touchedXY = (TextView) findViewById(R.id.xy);
         invertedXY = (TextView) findViewById(R.id.invertedxy);
         imgSize = (TextView) findViewById(R.id.size);
-        colorRGB = (TextView) findViewById(R.id.colorrgb);
-        imgSource1 = (ImageView) findViewById(R.id.source1);
-//        imgSource2 = (ImageView)findViewById(R.id.source2);
+        colorText = (TextView) findViewById(R.id.colorrgb);
+        colorBtn = (Button) findViewById(R.id.btn);
+        imgSource1 = (ImageView) findViewById(R.id.pic);
 
         imgSource1.setOnTouchListener(imgSourceOnTouchListener);
-//        imgSource2.setOnTouchListener(imgSourceOnTouchListener);
     }
 
     View.OnTouchListener imgSourceOnTouchListener = new View.OnTouchListener() {
 
+        // 将 drawable 格式转化为 bitmap 格式
         @NonNull
-        private Bitmap getBitmapFromDrawable(@NonNull Drawable drawable) {
+        private Bitmap drawableToBitamp(@NonNull Drawable drawable) {
             final Bitmap bmp = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
             final Canvas canvas = new Canvas(bmp);
             drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
             drawable.draw(canvas);
             return bmp;
-        }
-
-        private Bitmap drawableToBitamp(Drawable drawable)
-        {
-            int w = drawable.getIntrinsicWidth();
-            int h = drawable.getIntrinsicHeight();
-            Bitmap bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
-            return bitmap;
         }
 
         @Override
@@ -77,19 +69,19 @@ public class MainActivity extends AppCompatActivity {
                             + String.valueOf(eventY));
 
             invertedXY.setText(
-                    "touched position: "
+                    "tinverted touched position: "
                             + String.valueOf(x) + " / "
                             + String.valueOf(y));
 
             Drawable imgDrawable = ((ImageView) view).getDrawable();
-            Bitmap bitmap = getBitmapFromDrawable(imgDrawable);
+            Bitmap bitmap = drawableToBitamp(imgDrawable);
 
             imgSize.setText(
                     "drawable size: "
                             + String.valueOf(bitmap.getWidth()) + " / "
                             + String.valueOf(bitmap.getHeight()));
 
-            //Limit x, y range within bitmap
+            // 坐标超出图片范围
             if (x < 0) {
                 x = 0;
             } else if (x > bitmap.getWidth() - 1) {
@@ -109,9 +101,10 @@ public class MainActivity extends AppCompatActivity {
             String r1 = Integer.toHexString(r);
             String g1 = Integer.toHexString(g);
             String b1 = Integer.toHexString(b);
-            String strRGB = r1+g1+b1;
-            colorRGB.setText("touched color: " + "#" + strRGB);
-//            colorRGB.setTextColor(touchedRGB);
+            String strRGB = r1 + g1 + b1;
+            colorText.setText("touched color: " + "#" + strRGB);
+            colorText.setTextColor(touchedRGB);
+            colorBtn.setBackgroundColor(touchedRGB);
 
             return true;
         }
