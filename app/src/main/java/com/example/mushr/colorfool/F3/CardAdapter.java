4f03aa4156eeaadd680f598d31394208e6fcf06f
messengerW <messenger232624@gmail.com>
2019-10-06 22:47:27 +0800

    使用瀑布流式的RecyclerView

--- app/src/main/java/com/example/mushr/colorfool/F3/CardAdapter.java
+++ app/src/main/java/com/example/mushr/colorfool/F3/CardAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -43,7 +44,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.myViewHolder> 
             super(itemView);
             image = (ImageView) itemView.findViewById(R.id.card_pic);
 //            imageId = (TextView) itemView.findViewById();
-            userName = (TextView) itemView.findViewById(R.id.card_text);
+//            userName = (TextView) itemView.findViewById(R.id.card_text);
 //            userId = (TextView) itemView.findViewById();
             userHead = (ImageButton) itemView.findViewById(R.id.card_head);
 
@@ -58,12 +59,18 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.myViewHolder> 
 
     }
 
+    /* 这里有一个小坑，之前一直在用居然没踩到 ??
+     * 就是写布局时我发现recyclerview里面的card设置了match_parent但无论如何也顶不满整个布局的宽度，
+      * 后来上百度查到这是recylerview的一个小bug，这里需要稍微改一下这个 onCreateViewHolder 函数，
+      * 所以你看到的这和之前433里面的会稍有不同
+      * */
     @NonNull
     @Override
     public myViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         //创建自定义布局
-        View view = View.inflate(context, R.layout.card_item, null);
-        return new myViewHolder(view);
+        View inflate = LayoutInflater.from(context).inflate(R.layout.card_itemnew, viewGroup, false);
+        myViewHolder viewHolder = new myViewHolder(inflate);
+        return viewHolder;
     }
 
     @Override
@@ -72,7 +79,7 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.myViewHolder> 
 //        holder.imageId.setText(bean.imageId);
         holder.image.setImageDrawable(bean.image);
 //        holder.userId.setText(bean.userId);
-        holder.userName.setText(bean.userName);
+//        holder.userName.setText(bean.userName);
         holder.userHead.setBackground(CustomDrawableUtil.getRoundedBitmapDrawable(context,bean.userHead));
     }
 }
--- app/src/main/java/com/example/mushr/colorfool/F3/Fragment3.java
+++ app/src/main/java/com/example/mushr/colorfool/F3/Fragment3.java
@@ -1,18 +1,15 @@
 package com.example.mushr.colorfool.F3;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
-import android.widget.ImageButton;
 
 import com.example.mushr.colorfool.R;
 
@@ -51,13 +48,13 @@ public class Fragment3 extends Fragment {
 
     private void initRecyclerView(){
         recyclerView = (RecyclerView)view.findViewById(R.id.recyclerview);
+        recyclerView.setPadding(8,8,8,8);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+
         adapter = new CardAdapter(getActivity(),ItemList);
-
-
-
         recyclerView.setAdapter(adapter);
         //  设置recycler的样式
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
+        //  recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false));
         //  设置分割线
         //recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
 
